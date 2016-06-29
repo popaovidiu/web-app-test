6146091fb850c3037ade0231ae7910dab445b51c
popaovidiu <popaovidiu2004@gmail.com>
2016-06-29 19:33:56 +0300

    updates DB

--- src/main/java/ro/sci/ems/cfg/ApplicationConfiguration.java
+++ src/main/java/ro/sci/ems/cfg/ApplicationConfiguration.java
@@ -33,28 +33,43 @@ public class ApplicationConfiguration {
     	return new SecurityFilter();
     }
     
-	@Bean
-	public DataSource dataSource() {
-		BasicDataSource dataSource = new BasicDataSource();
-		dataSource.setDriverClassName("org.postgresql.Driver");
-		
-		String url = new StringBuilder()
-				.append("jdbc:")
-				.append("postgresql")
-				.append("://")
-				.append("localhost")
-				.append(":")
-				.append("5432")
-				.append("/")
-				.append("ems").toString();
-		
-		dataSource.setUrl(url);
-		dataSource.setUsername("postgres");
-		dataSource.setPassword("q1w2e3r4");
-		return dataSource;
-		
-	}
+//	@Bean
+//	public DataSource dataSource() {
+//		BasicDataSource dataSource = new BasicDataSource();
+//		dataSource.setDriverClassName("org.postgresql.Driver");
+//		
+//		String url = new StringBuilder()
+//				.append("jdbc:")
+//				.append("postgresql")
+//				.append("://")
+//				.append("localhost")
+//				.append(":")
+//				.append("5432")
+//				.append("/")
+//				.append("ems").toString();
+//		
+//		dataSource.setUrl(url);
+//		dataSource.setUsername("postgres");
+//		dataSource.setPassword("q1w2e3r4");
+//		return dataSource;
+//		
+//	}
 	
+    @Bean
+    public BasicDataSource dataSource() {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        String username = System.getenv("JDBC_DATABASE_USERNAME");
+        String password = System.getenv("JDBC_DATABASE_PASSWORD");
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+    
+    
 	@Bean
 	public EmployeeDAO employeeDAO() {
 		//return new IMEmployeeDAO();
